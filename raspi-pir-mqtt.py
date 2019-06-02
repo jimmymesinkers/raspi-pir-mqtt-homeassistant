bf0d60eb260ec8ebca073977785b6f876c2c6e42
James Sinclair <sinclair.j@gmail.com>
2019-06-02 20:16:15 +1000

    Removed use of set for setting as set is reserved

--- raspi-pir-mqtt.py
+++ raspi-pir-mqtt.py
@@ -10,23 +10,23 @@ import mqtt_settings as set
 # On motion: send motion payload
 
 def mqtt_message(payload):
-    publish.single(set.mqtt_channel,
+    publish.single(settings.mqtt_channel,
       payload=payload,
-      hostname=set.mqtt_hostname,
-      client_id=set.client_id,
-      auth=set.auth,
-      port=set.mqtt_port,
+      hostname=settings.mqtt_hostname,
+      client_id=settings.client_id,
+      auth=settings.auth,
+      port=settings.mqtt_port,
       protocol=mqtt.MQTTv311)
 
 def when_motion():
     print('Motion On')
-    mqtt_message(set.motion_detected)
+    mqtt_message(settings.motion_detected)
 
 def when_no_motion():
     print('Motion off')
-    mqtt_message(set.motion_undetected)
+    mqtt_message(settings.motion_undetected)
 
-pir = MotionSensor(set.pir_sensor_pin, queue_len=set.queue_length)
+pir = MotionSensor(settings.pir_sensor_pin, queue_len=settings.queue_length)
 pir.when_motion = when_motion
 pir.when_no_motion = when_no_motion
 pause()
